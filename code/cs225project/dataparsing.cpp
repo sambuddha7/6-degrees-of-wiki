c4e66581f03964f4f717061a546d1cc542e2d258
sambuddha7 <sbiswas7@illinois.edu>
2022-12-12 20:14:00 -0600

    data parsing files

---
+++ code/cs225project/dataparsing.cpp
@@ -0,0 +1,81 @@
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <map>
+#include <unordered_map>
+#include "dataparsing.h"
+#include <algorithm>
+#include "unordered_map"
+using namespace std;
+
+vector<vector<string> > file_to_vector_csv(const std::string & filename){
+    vector<vector<string> > to_return;
+    std::ifstream istream(filename);
+    string line;
+    while (std::getline(istream, line)) {
+        vector<string> vect;
+        SplitString(line, ',', vect);
+        for (unsigned i = 0; i < vect.size(); ++i) {
+            vect[i] = Trim(vect[i]);
+        }
+        to_return.push_back(vect);
+    }
+    
+    return to_return;
+}
+vector<vector<string> > file_to_vector_txt(const std::string & filename){
+    vector<vector<string> > to_return;
+    std::ifstream istream(filename);
+    string line;
+    while (std::getline(istream, line)) {
+        vector<string> vect;
+        SplitString(line, ' ', vect);
+        for (unsigned i = 0; i < vect.size(); ++i) {
+            vect[i] = Trim(vect[i]);
+        }
+        to_return.push_back(vect);
+    }
+    
+    return to_return;
+}
+
+Graph vector_to_graph(vector<vector<string> > csv, vector<vector<string> > txt)  {
+    Graph g;
+    unordered_map<string, string> id_to_name;
+    for (vector<string> vect : csv) {
+        id_to_name[vect[0]] = vect[1];
+    }
+    for (vector<string> vect : txt) {
+        Node node_one(id_to_name[vect[0]]);
+        Node node_two(id_to_name[vect[1]]);
+        cout << &node_one << endl;
+        g.addEdge(&node_one, &node_two);
+    }
+    return g;
+}
+
+std::string TrimRight(const std::string & str) {
+    std::string tmp = str;
+    return tmp.erase(tmp.find_last_not_of(" ") + 1);
+}
+
+std::string TrimLeft(const std::string & str) {
+    std::string tmp = str;
+    return tmp.erase(0, tmp.find_first_not_of(" "));
+}
+
+std::string Trim(const std::string & str) {
+    std::string tmp = str;
+    return TrimLeft(TrimRight(str));
+}
+
+void SplitString(const std::string & str1, char sep, std::vector<std::string> &fields) {
+    std::string str = str1;
+    std::string::size_type pos;
+    while((pos=str.find(sep)) != std::string::npos) {
+        fields.push_back(str.substr(0,pos));
+        str.erase(0,pos+1);  
+    }
+    fields.push_back(str);
+}
---
+++ code/cs225project/dataparsing.h
@@ -0,0 +1,23 @@
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <fstream>
+#include <sstream>
+#include <set>
+
+#include "BFS.cpp"
+using namespace std;
+
+vector<vector<string> > file_to_vector_csv(const std::string & filename);
+vector<vector<string> > file_to_vector_txt(const std::string & filename);
+Graph vector_to_graph(vector<vector<string> > csv, vector<vector<string> > txt);
+std::string file_to_string(const std::string & filename);
+std::string TrimRight(const std::string & str);
+std::string TrimLeft(const std::string & str);
+std::string Trim(const std::string & str);
+
+void SplitString(const std::string & str1, char sep, std::vector<std::string> &fields);
