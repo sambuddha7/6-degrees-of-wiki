a3502009cfa1d6e54c60a070a9fbcd9e2b5f335b
sambuddha7 <sbiswas7@illinois.edu>
2022-12-12 18:31:54 -0600

    Add files via upload

---
+++ code/cs225project/centrality.cpp
@@ -0,0 +1,19 @@
+#include <unordered_map>
+#include <list>
+#include <queue>
+#include <string>
+#include "BFS.cpp"
+#include <vector>
+using namespace std;
+class Graph {
+    
+    public:
+        unordered_map<Node*, list<Node*> > l;
+        void addEdge(Node* x, Node* y) {
+            l[x].push_back(y);
+            l[y].push_back(x);
+        }
+        int size() {
+            return l.size();
+        }
+};
